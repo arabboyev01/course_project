f141719f6c597fc06ae5a64969b146d2d8712ad1
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-09-14 20:18:48 +0500

    tags are fixed

--- src/Routes/Review/ReviewRoutes.ts
+++ src/Routes/Review/ReviewRoutes.ts
@@ -3,36 +3,38 @@ import { uploadImageToS3 } from '../../CloudService/connect';
 import { PrismaClient } from '.prisma/client';
 import { authenticateUser } from '../../AuthUser/AuthenticateUser'
 import { upload } from "../../utils/diskStorage"
+import { tagsQuery } from '../../utils/tagsQuery'
 
 const ReviewRoutes = express.Router();
 const prisma = new PrismaClient();
 
 ReviewRoutes.post('/', authenticateUser, upload.single('image'), async (req: Request | any, res: Response) => {
-    
+
     const { name, groupName, tags, reviewText, userId } = req.body;
     const tagsArray = JSON.parse(tags);
     const parseUser = parseInt(userId)
+    const tagIds: any = await tagsQuery(tagsArray)
 
     try {
-        const fileBuffer = req.file.buffer;
-        const originalFileName = req.file.originalname
+    const fileBuffer = req.file.buffer;
+    const originalFileName = req.file.originalname
 
-        const imageUrl: string = await uploadImageToS3(fileBuffer, originalFileName);
-     
-        const review = await prisma.review.create({
-            data: {
-                name,
-                groupName,
-                tags: {
-                    create: tagsArray.map((name: string) => ({ name })),
-                },
-                reviewText,
-                imageUrl,
-                userId: parseUser,
+    const imageUrl: string = await uploadImageToS3(fileBuffer, originalFileName);
+
+    const review = await prisma.review.create({
+        data: {
+            name,
+            groupName,
+            tags: {
+                connect: tagIds.map((tagId: number) => ({ id: tagId })),
             },
-        });
+            reviewText,
+            imageUrl,
+            userId: parseUser,
+        },
+    });
 
-        res.json(review);
+    res.json(review);
     } catch (error) {
         return res.status(500).json(error);
     }
---
+++ src/utils/tagsQuery.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '.prisma/client';
+
+const prisma = new PrismaClient();
+
+const tagsQuery = async (tagsArray: string[]) => {
+    const tagIds = [];
+
+    for (const tagName of tagsArray) {
+        const existingTag = await prisma.tag.findUnique({
+            where: {
+                name: tagName,
+            },
+        });
+
+        if (existingTag) {
+            tagIds.push(existingTag.id);
+        } else {
+            const newTag = await prisma.tag.create({
+                data: {
+                    name: tagName,
+                },
+            });
+            tagIds.push(newTag.id);
+        }
+    }
+
+    return tagIds;
+}
+
+export { tagsQuery };
