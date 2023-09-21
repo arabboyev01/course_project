59b4961c8069e3bfdc7fc311897ea8f5a708ed05
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-09-21 17:01:20 +0500

    ts-config changed

--- src/RedisConnection/index.ts
+++ src/RedisConnection/index.ts
@@ -1,14 +1,9 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  host: "singapore-redis.render.com",
-  port: 6379,
-  password: "jI8SAd97U6caKneT2CjVgemL8Dhuidv2",
-  tls: {
-    servername: "singapore-redis.render.com"
-  }
+  host: "redis://red-ck62v2b6fquc73c16lp0",
+  port: 6379
 })
-
 redis.ping((err, result) => {
     if (err) {
         console.error("Error connecting to Redis:", err);
